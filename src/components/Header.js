6207488fe9154a9c09fd462e877b8541ef8a27cf
Oihan <oaginagabo23wg@ikzubirimanteo.com>
2024-12-30 19:57:44 +0100

    logo zuzenketa

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import logo from './logo.png';
 
 const Header = () => {
   return (
     <header className="d-flex align-items-center mb-4">
       <h1 className="text-light col-lg-9">ZUBIRI MANTEO</h1>
       <div className="col-lg-3 ms-auto">
-        <img src="/logo.png" className="rounded-circle header-img" alt="Logo Zubiri Manteo" />
+        <img src={logo} className="rounded-circle header-img" alt="Logo Zubiri Manteo" />
       </div>
     </header>
   );
